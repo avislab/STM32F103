0a9624b99b1a2002527451eee85a5526f8fa0741
Andrey Koryagin <andre@avislab.com>
2016-09-22 13:30:59 +0300

    RTC Example

--- Example_RTC/main.c
+++ Example_RTC/main.c
@@ -7,6 +7,59 @@
 #include "stdio.h"
 #include "misc.h"
 
+#include "tim2_delay.h"
+
+void SetSysClockToHSE(void)
+{
+	ErrorStatus HSEStartUpStatus;
+
+	/* SYSCLK, HCLK, PCLK2 and PCLK1 configuration -----------------------------*/
+    /* RCC system reset(for debug purpose) */
+    RCC_DeInit();
+
+    /* Enable HSE */
+    RCC_HSEConfig( RCC_HSE_ON);
+
+    /* Wait till HSE is ready */
+    HSEStartUpStatus = RCC_WaitForHSEStartUp();
+
+    if (HSEStartUpStatus == SUCCESS)
+    {
+        /* Enable Prefetch Buffer */
+        //FLASH_PrefetchBufferCmd( FLASH_PrefetchBuffer_Enable);
+
+        /* Flash 0 wait state */
+        //FLASH_SetLatency( FLASH_Latency_0);
+
+        /* HCLK = SYSCLK */
+        RCC_HCLKConfig( RCC_SYSCLK_Div1);
+
+        /* PCLK2 = HCLK */
+        RCC_PCLK2Config( RCC_HCLK_Div1);
+
+        /* PCLK1 = HCLK */
+        RCC_PCLK1Config(RCC_HCLK_Div1);
+
+        /* Select HSE as system clock source */
+        RCC_SYSCLKConfig( RCC_SYSCLKSource_HSE);
+
+        /* Wait till PLL is used as system clock source */
+        while (RCC_GetSYSCLKSource() != 0x04)
+        {
+        }
+    }
+    else
+    { /* If HSE fails to start-up, the application will have wrong clock configuration.
+     User can add here some code to deal with this error */
+
+        /* Go to infinite loop */
+        while (1)
+        {
+        }
+    }
+}
+
+
 void usart_init(void)
 {
 	    /* Enable USART1 and GPIOA clock */
@@ -113,18 +166,18 @@ typedef struct
 	uint8_t RTC_Minutes;
 	uint8_t RTC_Seconds;
 	uint8_t RTC_Date;
+	uint8_t RTC_Wday;
 	uint8_t RTC_Month;
 	uint16_t RTC_Year;
 } RTC_DateTimeTypeDef;
 
-// Convert Counter to Date
-void RTC_GetDateTime(uint32_t RTC_Counter, RTC_DateTimeTypeDef* RTC_DateTimeuct)
-{
+// Get current date
+void RTC_GetDateTime(uint32_t RTC_Counter, RTC_DateTimeTypeDef* RTC_DateTimeStruct) {
 	unsigned long time;
 	unsigned long t1, a, b, c, d, e, m;
 	int year = 0;
 	int mon = 0;
-	//int wday = 0;
+	int wday = 0;
 	int mday = 0;
 	int hour = 0;
 	int min = 0;
@@ -147,7 +200,7 @@ void RTC_GetDateTime(uint32_t RTC_Counter, RTC_DateTimeTypeDef* RTC_DateTimeuct)
 	t1 = time/24;
 	hour = time - t1*24;
 
-	//wday = jdn%7;
+	wday = jdn%7;
 
 	a = jdn + 32044;
 	b = (4*a+3)/146097;
@@ -159,27 +212,27 @@ void RTC_GetDateTime(uint32_t RTC_Counter, RTC_DateTimeTypeDef* RTC_DateTimeuct)
 	mon = m + 3 - 12*(m/10);
 	year = 100*b + d - 4800 + (m/10);
 
-	RTC_DateTimeuct->RTC_Year = year;
-	RTC_DateTimeuct->RTC_Month = mon;
-	RTC_DateTimeuct->RTC_Date = mday;
-	RTC_DateTimeuct->RTC_Hours = hour;
-	RTC_DateTimeuct->RTC_Minutes = min;
-	RTC_DateTimeuct->RTC_Seconds = sec;
-	//RTC_DateTimeuct->dow = wday;
+	RTC_DateTimeStruct->RTC_Year = year;
+	RTC_DateTimeStruct->RTC_Month = mon;
+	RTC_DateTimeStruct->RTC_Date = mday;
+	RTC_DateTimeStruct->RTC_Hours = hour;
+	RTC_DateTimeStruct->RTC_Minutes = min;
+	RTC_DateTimeStruct->RTC_Seconds = sec;
+	RTC_DateTimeStruct->RTC_Wday = wday;
 }
 
 // Convert Date to Counter
-uint32_t RTC_GetRTC_Counter(RTC_DateTimeTypeDef* RTC_DateTimeuct) {
+uint32_t RTC_GetRTC_Counter(RTC_DateTimeTypeDef* RTC_DateTimeStruct) {
 	uint8_t a;
 	uint16_t y;
 	uint8_t m;
 	uint32_t JDN;
 
-	a=(14-RTC_DateTimeuct->RTC_Month)/12;
-	y=RTC_DateTimeuct->RTC_Year+4800-a;
-	m=RTC_DateTimeuct->RTC_Month+(12*a)-3;
+	a=(14-RTC_DateTimeStruct->RTC_Month)/12;
+	y=RTC_DateTimeStruct->RTC_Year+4800-a;
+	m=RTC_DateTimeStruct->RTC_Month+(12*a)-3;
 
-	JDN=RTC_DateTimeuct->RTC_Date;
+	JDN=RTC_DateTimeStruct->RTC_Date;
 	JDN+=(153*m+2)/5;
 	JDN+=365*y;
 	JDN+=y/4;
@@ -188,11 +241,42 @@ uint32_t RTC_GetRTC_Counter(RTC_DateTimeTypeDef* RTC_DateTimeuct) {
 	JDN = JDN -32045;
 	JDN = JDN - JULIAN_DATE_BASE;
 	JDN*=86400;
-	JDN+=(RTC_DateTimeuct->RTC_Hours*3600);
-	JDN+=(RTC_DateTimeuct->RTC_Minutes*60);
-	JDN+=(RTC_DateTimeuct->RTC_Seconds);
+	JDN+=(RTC_DateTimeStruct->RTC_Hours*3600);
+	JDN+=(RTC_DateTimeStruct->RTC_Minutes*60);
+	JDN+=(RTC_DateTimeStruct->RTC_Seconds);
 
 	return JDN;
+}
+
+void RTC_GetMyFormat(RTC_DateTimeTypeDef* RTC_DateTimeStruct, char * buffer) {
+	const char WDAY0[] = "Monday";
+	const char WDAY1[] = "Tuesday";
+	const char WDAY2[] = "Wednesday";
+	const char WDAY3[] = "Thursday";
+	const char WDAY4[] = "Friday";
+	const char WDAY5[] = "Saturday";
+	const char WDAY6[] = "Sunday";
+	const char * WDAY[7]={WDAY0, WDAY1, WDAY2, WDAY3, WDAY4, WDAY5, WDAY6};
+
+	const char MONTH1[] = "January";
+	const char MONTH2[] = "February";
+	const char MONTH3[] = "March";
+	const char MONTH4[] = "April";
+	const char MONTH5[] = "May";
+	const char MONTH6[] = "June";
+	const char MONTH7[] = "July";
+	const char MONTH8[] = "August";
+	const char MONTH9[] = "September";
+	const char MONTH10[] = "October";
+	const char MONTH11[] = "November";
+	const char MONTH12[] = "December";
+	const char * MONTH[12]={MONTH1, MONTH2, MONTH3, MONTH4, MONTH5, MONTH6, MONTH7, MONTH8, MONTH9, MONTH10, MONTH11, MONTH12};
+
+	sprintf(buffer, "%s %d %s %04d",
+			WDAY[RTC_DateTimeStruct->RTC_Wday],
+			RTC_DateTimeStruct->RTC_Date,
+			MONTH[RTC_DateTimeStruct->RTC_Month],
+			RTC_DateTimeStruct->RTC_Year);
 }
 //========================================================================================
 
@@ -202,30 +286,41 @@ int main(void)
 	uint32_t RTC_Counter = 0;
 	RTC_DateTimeTypeDef	RTC_DateTime;
 
+	SetSysClockToHSE();
+	TIM2_init();
+
 	usart_init();
+
 	if (RTC_Init() == 1) {
-		// Якщо перша ініціалізіція RTC Встановлюємо початкову дату, наприклад 14.10.2016 14:35:17
-		RTC_DateTime.RTC_Date = 14;
-		RTC_DateTime.RTC_Month = 10;
-		RTC_DateTime.RTC_Year = 10;
+		// Якщо перша ініціалізація RTC Встановлюємо початкову дату, наприклад 22.09.2016 14:30:00
+		RTC_DateTime.RTC_Date = 22;
+		RTC_DateTime.RTC_Month = 9;
+		RTC_DateTime.RTC_Year = 2016;
 
 		RTC_DateTime.RTC_Hours = 14;
-		RTC_DateTime.RTC_Minutes = 35;
-		RTC_DateTime.RTC_Seconds = 17;
+		RTC_DateTime.RTC_Minutes = 30;
+		RTC_DateTime.RTC_Seconds = 00;
 
+		// Після ініціалізації потрібна затримка. Без неї час не встановлюється.
+		delay_ms(500);
 		RTC_SetCounter(RTC_GetRTC_Counter(&RTC_DateTime));
 	}
 
-    while(1)
+	while(1)
     {
     	RTC_Counter = RTC_GetCounter();
-    	sprintf(buffer, "COUNTER: %d\r\n", (int)RTC_Counter);
+    	sprintf(buffer, "\r\n\r\nCOUNTER: %d\r\n", (int)RTC_Counter);
 		USARTSend(buffer);
 
 		RTC_GetDateTime(RTC_Counter, &RTC_DateTime);
 		sprintf(buffer, "%02d.%02d.%04d  %02d:%02d:%02d\r\n",
 				RTC_DateTime.RTC_Date, RTC_DateTime.RTC_Month, RTC_DateTime.RTC_Year,
 				RTC_DateTime.RTC_Hours, RTC_DateTime.RTC_Minutes, RTC_DateTime.RTC_Seconds);
+		USARTSend(buffer);
+
+		// Функція генерує у буфері дату власного формату
+		RTC_GetMyFormat(&RTC_DateTime, buffer);
+		USARTSend(buffer);
 
 		/* delay */
 		while (RTC_Counter == RTC_GetCounter()) {
