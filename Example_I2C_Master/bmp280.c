3e15dedc8ef93307dba37f3d3cadf770c66d3180
Andrey Koryagin <andre@avislab.com>
2016-10-06 13:56:57 +0300

    I2C Master (BMP280)

---
+++ Example_I2C_Master/bmp280.c
@@ -0,0 +1,173 @@
+#include "stm32f10x_i2c.h"
+#include "bmp280.h"
+#include <math.h>
+
+unsigned short dig_T1;
+short dig_T2;
+short dig_T3;
+unsigned short dig_P1;
+
+short dig_P2;
+short dig_P3;
+short dig_P4;
+short dig_P5;
+short dig_P6;
+short dig_P7;
+short dig_P8;
+short dig_P9;
+
+short bmp280ReadShort(unsigned char address)
+{
+  short msb=0;
+  short lsb=0;
+
+  I2C_AcknowledgeConfig(I2C1,ENABLE);
+  I2C_GenerateSTART(I2C1,ENABLE);
+
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
+  I2C_Send7bitAddress(I2C1, BMP280_addr, I2C_Direction_Transmitter);
+
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+
+  I2C_SendData(I2C1,address);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+
+  I2C_GenerateSTART(I2C1,ENABLE);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
+
+  I2C_Send7bitAddress(I2C1, BMP280_addr, I2C_Direction_Receiver);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
+
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED));
+  lsb = I2C_ReceiveData(I2C1);
+
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED));
+  msb = I2C_ReceiveData(I2C1);
+
+  I2C_GenerateSTOP(I2C1,ENABLE);
+  I2C_AcknowledgeConfig(I2C1,DISABLE);
+
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED));
+  I2C_ReceiveData(I2C1);
+
+  return (msb << 8) | lsb;
+}
+
+unsigned long bmp280ReadLong(unsigned char address)
+{
+  unsigned long result=0;
+
+  unsigned long msb=0;
+  unsigned long lsb=0;
+  unsigned long xsb=0;
+
+  I2C_AcknowledgeConfig(I2C1,ENABLE);
+  I2C_GenerateSTART(I2C1,ENABLE);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
+
+  I2C_Send7bitAddress(I2C1, BMP280_addr, I2C_Direction_Transmitter);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+
+  I2C_SendData(I2C1,address);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+
+  I2C_GenerateSTART(I2C1,ENABLE);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
+
+  I2C_Send7bitAddress(I2C1, BMP280_addr, I2C_Direction_Receiver);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
+
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED));
+  msb = I2C_ReceiveData(I2C1);
+
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED));
+  lsb = I2C_ReceiveData(I2C1);
+
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED));
+  xsb = I2C_ReceiveData(I2C1);
+
+  I2C_GenerateSTOP(I2C1,ENABLE);
+  I2C_AcknowledgeConfig(I2C1,DISABLE);
+  
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED));
+  I2C_ReceiveData(I2C1);
+
+  result = (msb << 16) | (lsb << 8) | xsb;
+  
+  return (result >> 4);
+}
+
+void bmp280WriteByte(unsigned char address, unsigned char data)
+{
+  I2C_GenerateSTART(I2C1,ENABLE);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
+
+  I2C_Send7bitAddress(I2C1, BMP280_addr, I2C_Direction_Transmitter);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+
+  I2C_SendData(I2C1,address);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+
+  I2C_SendData(I2C1,data);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+
+  I2C_GenerateSTOP(I2C1,ENABLE);
+
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+}
+//----------------------------------------
+
+void bmp280Convert(long* temperature, long* pressure)
+{
+  unsigned long adc_T;
+  unsigned long adc_P;
+  adc_T = bmp280ReadLong(BMP280_REG_RESULT_TEMPRERATURE);
+  adc_P = bmp280ReadLong(BMP280_REG_RESULT_PRESSURE);
+
+  double var1, var2, p, t_fine;
+  var1 = (((double)adc_T)/16384.0 - ((double)dig_T1)/1024.0) * ((double)dig_T2);
+  var2 = ((((double)adc_T)/131072.0 - ((double)dig_T1)/8192.0) * (((double)adc_T)/131072.0 - ((double) dig_T1)/8192.0)) * ((double)dig_T3);
+  t_fine = (var1 + var2);
+  *temperature = (int32_t) (t_fine*10 / 5120.0);
+
+  var1 = ((double)t_fine/2.0) - 64000.0;
+  var2 = var1 * var1 * ((double)dig_P6) / 32768.0;
+  var2 = var2 + var1 * ((double)dig_P5) * 2.0;
+  var2 = (var2/4.0)+(((double)dig_P4) * 65536.0);
+  var1 = (((double)dig_P3) * var1 * var1 / 524288.0 + ((double)dig_P2) * var1) / 524288.0;
+  var1 = (1.0 + var1 / 32768.0)*((double)dig_P1);
+  if (var1 == 0.0)
+  {
+    return; // avoid exception caused by division by zero
+  }
+  p = 1048576.0 - (double)adc_P;
+  p = (p - (var2 / 4096.0)) * 6250.0 / var1;
+  var1 = ((double)dig_P9) * p * p / 2147483648.0;
+  var2 = p * ((double)dig_P8) / 32768.0;
+  p = (p + (var1 + var2 + ((double)dig_P7)) / 16.0);
+
+  *pressure = (long)p;
+}
+//----------------------------------------
+
+void BMP280_Init(void)
+{ 
+  dig_T1 = bmp280ReadShort(0x88);//dig_T1
+  dig_T2 = bmp280ReadShort(0x8A);//dig_T2
+  dig_T3 = bmp280ReadShort(0x8C);//dig_T3
+  dig_P1 = bmp280ReadShort(0x8E);//dig_P1
+  dig_P2 = bmp280ReadShort(0x90);//dig_P2
+  dig_P3 = bmp280ReadShort(0x92);//dig_P3
+  dig_P4 = bmp280ReadShort(0x94);//dig_P4
+  dig_P5 = bmp280ReadShort(0x96);//dig_P5
+  dig_P6 = bmp280ReadShort(0x98);//dig_P6
+  dig_P7 = bmp280ReadShort(0x9A);//dig_P7
+  dig_P8 = bmp280ReadShort(0x9C);//dig_P8
+  dig_P9 = bmp280ReadShort(0x9E);//dig_P9
+
+  bmp280WriteByte(BMP280_REG_CONFIG, BMP280_CONFIG);
+  bmp280WriteByte(BMP280_REG_CONTROL, BMP280_MEAS);
+}
+
+//----------------------------------------
+
---
+++ Example_I2C_Master/main.c
@@ -0,0 +1,216 @@
+#include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_usart.h"
+#include "stm32f10x_dma.h"
+#include "stm32f10x_tim.h"
+#include "stm32f10x_i2c.h"
+#include "misc.h"
+#include "stdio.h"
+#include "string.h"
+
+#include "bmp280.h"
+
+volatile unsigned char FLAG_USART;
+
+/*******************************************************************/
+// I2C
+/*******************************************************************/
+void I2C1_init(void)
+{
+    I2C_InitTypeDef  I2C_InitStructure;
+    GPIO_InitTypeDef  GPIO_InitStructure;
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+
+    /* Configure I2C_EE pins: SCL and SDA */
+    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_6 | GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+    /* I2C configuration */
+    I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
+    I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
+    I2C_InitStructure.I2C_OwnAddress1 = 0x38;
+    I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
+    I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+    I2C_InitStructure.I2C_ClockSpeed = 100000;
+
+    /* I2C Peripheral Enable */
+    I2C_Cmd(I2C1, ENABLE);
+    /* Apply I2C configuration after enabling it */
+    I2C_Init(I2C1, &I2C_InitStructure);
+}
+
+
+/*******************************************************************/
+
+/*******************************************************************/
+// UART DMA
+/*******************************************************************/
+char usart_buffer[80] = {'\0'};
+/*******************************************************************/
+void usart_dma_init(void)
+{
+	/* Enable USART1 and GPIOA clock */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA, ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+
+	/* DMA */
+	DMA_InitTypeDef DMA_InitStruct;
+	DMA_InitStruct.DMA_PeripheralBaseAddr = (uint32_t)&(USART1->DR);
+	DMA_InitStruct.DMA_MemoryBaseAddr = (uint32_t)&usart_buffer[0];
+	DMA_InitStruct.DMA_DIR = DMA_DIR_PeripheralDST;
+	DMA_InitStruct.DMA_BufferSize = sizeof(usart_buffer);
+	DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	DMA_InitStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+	DMA_InitStruct.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+	DMA_InitStruct.DMA_Mode = DMA_Mode_Normal;
+	DMA_InitStruct.DMA_Priority = DMA_Priority_Low;
+	DMA_InitStruct.DMA_M2M = DMA_M2M_Disable;
+	DMA_Init(DMA1_Channel4, &DMA_InitStruct);
+
+	/* NVIC Configuration */
+	NVIC_InitTypeDef NVIC_InitStructure;
+	/* Enable the USARTx Interrupt */
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	/* Configure the GPIOs */
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	/* Configure USART1 Tx (PA.09) as alternate function push-pull */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure USART1 Rx (PA.10) as input floating */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure the USART1 */
+	USART_InitTypeDef USART_InitStructure;
+
+	/* USART1 configuration ------------------------------------------------------*/
+	/* USART1 configured as follow:
+		- BaudRate = 115200 baud
+		- Word Length = 8 Bits
+		- One Stop Bit
+		- No parity
+		- Hardware flow control disabled (RTS and CTS signals)
+		- Receive and transmit enabled
+		- USART Clock disabled
+		- USART CPOL: Clock is active low
+		- USART CPHA: Data is captured on the middle
+		- USART LastBit: The clock pulse of the last data bit is not output to
+			the SCLK pin
+	 */
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	//USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_InitStructure.USART_Mode = USART_Mode_Tx;
+
+	USART_Init(USART1, &USART_InitStructure);
+
+	/* Enable USART1 */
+	USART_Cmd(USART1, ENABLE);
+
+	USART_DMACmd(USART1, USART_DMAReq_Tx, ENABLE);
+	//DMA_Cmd(DMA1_Channel4, ENABLE);
+
+	DMA_ITConfig(DMA1_Channel4, DMA_IT_TC, ENABLE);
+	NVIC_EnableIRQ(DMA1_Channel4_IRQn);
+}
+
+/*******************************************************************/
+void USARTSendDMA(char *pucBuffer)
+{
+	strcpy(usart_buffer, pucBuffer);
+
+	/* Restart DMA Channel*/
+	DMA_Cmd(DMA1_Channel4, DISABLE);
+	DMA1_Channel4->CNDTR = strlen(pucBuffer);
+	DMA_Cmd(DMA1_Channel4, ENABLE);
+}
+
+/*******************************************************************/
+void DMA1_Channel4_IRQHandler(void)
+{
+	DMA_ClearITPendingBit(DMA1_IT_TC4);
+	DMA_Cmd(DMA1_Channel4, DISABLE);
+}
+/*******************************************************************/
+
+/*******************************************************************/
+// TIM4
+/*******************************************************************/
+void TIM4_init(void) {
+	// TIMER4
+    TIM_TimeBaseInitTypeDef TIMER_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+  	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+
+  	TIM_TimeBaseStructInit(&TIMER_InitStructure);
+    TIMER_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIMER_InitStructure.TIM_Prescaler = 8000;
+    TIMER_InitStructure.TIM_Period = 10000;
+    TIM_TimeBaseInit(TIM4, &TIMER_InitStructure);
+    TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
+    TIM_Cmd(TIM4, ENABLE);
+
+    /* NVIC Configuration */
+    /* Enable the TIM4_IRQn Interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+}
+
+/*******************************************************************/
+void TIM4_IRQHandler(void)
+{
+	if (TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET)
+	{
+		TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
+
+		FLAG_USART = 1;
+	}
+}
+/*******************************************************************/
+
+int main(void)
+{
+	char buffer[80] = {'\0'};
+
+	long temperature = 0;
+	long pressure = 0;
+
+	I2C1_init();
+	BMP280_Init();
+	usart_dma_init();
+
+	TIM4_init();
+
+    while(1)
+    {
+		if (FLAG_USART == 1) {
+			bmp280Convert(&temperature, &pressure);
+			sprintf(buffer, "Temperature: %d, Pressure: %d\r\n", (int)temperature/10, (int)pressure);
+    		USARTSendDMA(buffer);
+			FLAG_USART = 0;
+    	}
+    }
+}
